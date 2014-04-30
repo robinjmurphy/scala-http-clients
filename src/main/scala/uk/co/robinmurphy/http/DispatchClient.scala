735b9f61878da48ae3496e2a5980785f18532cfc
Robin Murphy <robin@robinmurphy.co.uk>
2014-04-30 19:25:48 +0100

    Refactor response mapping in DispatchClient

--- src/main/scala/uk/co/robinmurphy/http/DispatchClient.scala
+++ src/main/scala/uk/co/robinmurphy/http/DispatchClient.scala
@@ -9,26 +9,28 @@ import scala.collection.JavaConverters._
 
 class DispatchClient extends AsyncHttpClient {
 
+  def get(url: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
+    Http(Url(url) <:< headers <<? params).map(responseFromDispatchResponse)
+  }
+
+  def put(url: String, body: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
+    Http(Url(url)).map(responseFromDispatchResponse)
+  }
+
+  def post(url: String, body: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
+    Http(Url(url)).map(responseFromDispatchResponse)
+  }
+
+  def delete(url: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
+    Http(Url(url)).map(responseFromDispatchResponse)
+  }
+
   private def headersFromDispatchHeaders(headers: FluentCaseInsensitiveStringsMap): Map[String, String] = {
     headers.entrySet.asScala.toList
       .foldLeft(Map[String, String]())((map, header) => map ++ Map(header.getKey -> header.getValue.asScala.mkString(", ")))
   }
 
-  def get(url: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
-    Http(Url(url) <:< headers <<? params).map { response =>
-      Response(response.getStatusCode, response.getResponseBody, headersFromDispatchHeaders(response.getHeaders))
-    }
-  }
-
-  def put(url: String, body: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
-    Http(Url(url)).map(response => Response(response.getStatusCode, response.getResponseBody))
-  }
-
-  def post(url: String, body: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
-    Http(Url(url)).map(response => Response(response.getStatusCode, response.getResponseBody))
-  }
-
-  def delete(url: String, params: Map[String, String], headers: Map[String, String]): Future[Response] = {
-    Http(Url(url)).map(response => Response(response.getStatusCode, response.getResponseBody))
+  private def responseFromDispatchResponse(response: dispatch.Res): Response = {
+    Response(response.getStatusCode, response.getResponseBody, headersFromDispatchHeaders(response.getHeaders))
   }
 }
